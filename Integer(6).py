e0b325b25439f45b6ce2e3344f485e2d1571c6e0
Krishna Kumar <nnkrishna714@gmail.com>
2023-01-24 12:41:48 +0530

    how are you

---
+++ Integer(6).py
@@ -0,0 +1,17 @@
+#Integer --> [0,1,2,3,4,5,6,7,8,9]
+#integer constructor is int()
+#integer literal is 0
+
+a=10
+print(a)
+
+b=int()
+print(b) #create an integer using constructor which is int()
+
+c=0
+print(type(c))
+
+
+d=32421345234523453456
+print(type(d),d,sep=" ")
+print(type(d),d)
---
+++ String(8).py
@@ -0,0 +1,100 @@
+#string
+# string's constructor is str()
+#string's literal is " ",' ',""" """,''' '''.
+# string is a set of sequence of character
+# string support indexing
+#string support slicing
+#string is immutable.
+
+a="Global"
+print(a)
+b='Global'
+print(b)
+c="""Global"""
+print(c)
+d='''Global'''
+print(d)
+
+#creating empty string using either string literal or constructor
+e=str()
+print("e",e,type(e),id(e))
+
+#literal for empty string
+f=''
+print(f,id(f))
+g=""
+print(g,id(g))
+
+h=""""""
+print(h,id(h))
+
+i=''''''
+print(i,id(i))
+
+#Indexing
+
+j="GLOBAL"
+print(j[2])  #'O'
+print(j[3])  #'B'
+print("CHIPS"[3])
+
+#Negative Indexing
+print(j[0])
+print(j[-6])
+print(j[3:])
+
+print(j[3::1])
+
+a="GLOBAL"
+print("Positive Indexing :",type(a[0]),a[0])
+print("Negative Indexing :",type(a[-6]),a[-6])
+
+#Slicing
+a="GLOBAL"
+
+b=a[10000:50000]
+print(b,end="\n")   #empty
+
+c=a[1:6:1]
+print(c)     #LOBAL
+
+
+d=a[1:6]
+print(d)
+
+
+e=a[-5:-1:1]
+print(e)
+
+
+f=a[-5:-1:-1]
+print(f)       #empty
+
+g=a[0:6:1]
+print(g)    #global
+
+h=a[::1]
+print(h)
+
+i=a[::]
+print(i)
+j=a[:]
+print(j)
+
+k=a[-5:-1:1]
+print(k)
+
+l=a[-6::1]
+print(l)
+
+m=a[-1:-7:-1]
+print(m)    #LABOLG
+
+n=a[:-7:-1] #LABOLG
+print("n",n)
+
+o=a[-1::-1] #LABOLG
+print("o",o)
+
+p=a[::-1]
+print("p",p)
